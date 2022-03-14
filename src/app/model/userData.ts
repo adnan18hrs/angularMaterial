e5ed11ae10a015294fd4e1658985b7d0bd7ea40b
adnan <khan.adnan346@gmail.com>
2022-03-14 13:36:52 +0530

    ..

--- src/app/model/userData.ts
+++ src/app/model/userData.ts
@@ -12,18 +12,4 @@ export class UserData{
     isAdmin:boolean;
     isUser:boolean;
     isMod:boolean;
-
-    /*constructor(response) {
-        this.username = response['username'];
-        this.token = response['token'];
-        this.email = response['email'];
-        this.roles = response['roles'];
-        this.id = response['id'];
-        this.type = response['type'];
-        this.message = response['message'];
-    }*/
-
-    /*getSalary() : number {
-        return 10000;
-    }*/
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { LoggingData } from 'src/app/model/loggingData';
 import { HomeService } from 'src/app/modules/home.service';
+import { HttpClient } from "@angular/common/http";
+import { Product } from 'src/app/model/product';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  headers:any; 
+
+  headers:any;
+  products: Product[]=[];
+  public oneProduct:Product;
   public popup:boolean;
-  constructor(private homeService:HomeService) { }
+  constructor(private homeService:HomeService,private httpClient: HttpClient) { }
   data: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  //students: Student[] = studentsData; 
   
   validateEmail(email:any) {
     return true;
@@ -31,10 +38,29 @@ export class HomeComponent implements OnInit {
     }
     else{alert("Please insert correct Email Id");}
   }
-
   
   ngOnInit() {
+    this.httpClient.get("assets/product.json").subscribe(data =>{
+      for (let index in data) {
+        this.converter(data[index]);
+      }
+      console.log("products = ",this.products);
+    })
     this.headers = this.homeService.getHttpHeader(); //this.myList.filter=""; this.myList.data.length=0;
+  }
+  
+  converter(p:any){
+    this.oneProduct = new Product();
+    this.oneProduct.name = p['name'];
+    this.oneProduct.image = p['image'];
+    this.oneProduct.description = p['description'];
+    this.oneProduct.brand = p['brand'];
+    this.oneProduct.category = p['category'];
+    this.oneProduct.price = p['price'];
+    this.oneProduct.countInStock = p['countInStock'];
+    this.oneProduct.rating = p['rating'];
+    this.oneProduct.numReviews = p['numReviews'];;
+    this.products.push(this.oneProduct);
   }
 
 }
