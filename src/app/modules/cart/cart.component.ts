f7ef9fc877f2176212035699f04779cc60696667
adnan <khan.adnan346@gmail.com>
2022-03-16 20:49:24 +0530

    ..

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  public cartItems:Product[]=[];
   constructor() { }
 
   ngOnInit() {
+    if(JSON.parse(localStorage.getItem("currentCart"))!=null){
+      console.log("cart is not empty...........");
+      this.cartItems = JSON.parse(localStorage.getItem("currentCart"));
+      console.log("cartItems = ",this.cartItems);
+    }
   }
-
+  counter(x:number) {
+    return new Array(x);
+  }
 }
